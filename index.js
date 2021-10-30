886296178e797e5ae3c54ec4f0eafc9c655d9a87
abedinforhane <farhanengn@gmail.comm>
2021-10-30 17:35:41 +0600

    first commit

--- index.js
+++ index.js
@@ -11,11 +11,11 @@ const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
-const port = process.eventNames  ||5000;
+const port = process.env.PORT ||5000;
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
